6793222074692bac1d59046b607d6a5253308d35
Petemab <peteben18@hotmail.com>
2018-05-30 19:02:14 +0100

    Bit more styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
       <div className="columns is-multiline is-mobile">
         <div className="column is-three-fifths is-offset-one-fifth has-text-centered">
           <h1 className="title has-text-white">Stand Up Soirée</h1>
-          <h2 className="subtitle has-text-white">Funny how?</h2>
+          <h2 className="subtitle has-text-white">Little description here</h2>
         </div>
         <div className="column is-one-third">
           <h1 className="title has-text-white">Stand Up Soirée</h1>
